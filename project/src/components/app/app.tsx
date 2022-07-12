1c71687475a2b09603fa77fcd26d3463a8178348
macspark <delwexx@gmail.com>
2022-07-13 00:54:18 +0400

    Добавлена маршрутизация

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,14 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {AppRoute, AuthorizationStatus} from '../../const';
+import PrivateRoute from '../private-route/private-route';
+import FavoritesScreen from '../../pages/favorites/favorites';
+//import FavoritesEmptyScreen from '../../pages/favorites-empty/favorites-empty';
+import LoginScreen from '../../pages/login/login';
 import MainScreen from '../../pages/main/main';
+//import MainEmptyScreen from '../../pages/main-empty/main-empty';
+import PropertyScreen from '../../pages/property/property';
+//import PropertyNotLoggedScreen from '../../pages/property-not-logged/property-not-logged';
+import NotFoundScreen from '../../pages/not-found/not-found';
 
 type AppProps = {
   offersCount: number;
@@ -6,9 +16,36 @@ type AppProps = {
 
 function App({offersCount}: AppProps): JSX.Element {
   return (
-    <MainScreen
-      offersCount={offersCount}
-    />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<MainScreen offersCount={offersCount} />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={<FavoritesScreen />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginScreen />}
+        />
+        <Route
+          path={AppRoute.Offer}
+          element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            >
+              <PropertyScreen />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="*"
+          element={<NotFoundScreen />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
