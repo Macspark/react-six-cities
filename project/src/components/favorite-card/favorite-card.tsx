d98b77d4985885ef90a7cbdc5332a9f2b1ada33a
macspark <delwexx@gmail.com>
2022-07-20 16:32:30 +0400

    fix

--- project/src/components/favorite-card/favorite-card.tsx
+++ project/src/components/favorite-card/favorite-card.tsx
@@ -1,12 +1,13 @@
 import {Offer} from '../../types/offer';
 import {Link} from 'react-router-dom';
+import {getRatingWidth} from '../../util';
 
 type FavoriteCardProps = {
   offer: Offer;
 }
 
 function FavoriteCard({offer}: FavoriteCardProps): JSX.Element {
-  const ratingWidth = `${Math.round(offer.rating) * 20}%`;
+  const ratingWidth = getRatingWidth(offer.rating);
   const offerLink = `/offer/${offer.id}`;
 
   return (
